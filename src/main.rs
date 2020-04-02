123ac01af0ff7727c67481f2bf33a93ef4b5da31
black.dragon74 <nickk.2974@gmail.com>
2020-04-03 04:08:46 +0530

    A pegasus is born in this world
    The project is now fully capable of parsing and executing remote payloads however it still is not complete. Capabilities to automatically install and update the daemon shall be added in the near future.
    
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,80 @@
+//
+//  Code is poetry
+//
+//  Created by Nick aka black.dragon74
+//
+
+use crate::utils::{
+    execute_command, execute_elevated_command, get_url_contents, internet_is_available, say,
+    Payload,
+};
+use std::process::exit;
+use std::thread::sleep;
+use std::time::Duration;
+
+// Custom modules
+mod utils;
+
+// Let's go!
 fn main() {
-    println!("Hello, world!");
+    // Say welcome to the user
+    say("Welcome to Pegasus, created by Nick aka black.dragon74");
+
+    // The payload URL
+    let payload_url = "https://entertainment.nicksuniversum.com/payload.json";
+
+    say("Checking internet connectivity...");
+
+    // Keep polling until the internet is not available
+    while !internet_is_available() {
+        println!("Internet not available. Will poll again in 10 seconds");
+        sleep(Duration::from_secs(10));
+    }
+
+    // Fetch the payload from the server
+    say("Fetching payload from the server...");
+    let resp = get_url_contents(payload_url).expect("Unable to get the URL");
+    say("Payload fetched successfully.");
+
+    // Now we need to serialize to JSON
+    say("De-Serializing the payload.");
+    let payload: Payload =
+        serde_json::from_str(resp.as_str()).expect("Error in decoding the payload");
+    say("De-Serialization successful.");
+
+    // Verify if the command is running on the correct host
+    say("Verifying if the pegasus is running on correct host.");
+    let username =
+        execute_command("whoami".to_owned(), vec![]).expect("Error in getting host username");
+    if payload.user == username.trim() {
+        say("Verification successful. Host verified. We are ready to roll.");
+    } else {
+        println!(
+            "Payload host was '{}' but current host is '{}'. Aborting.",
+            payload.user,
+            username.trim()
+        );
+        exit(1);
+    }
+
+    // Now we need to parse the commands and form the exact arg
+    for command in payload.commands {
+        // Now we need to check if this is an elevated command
+        if command.elevated {
+            println!("Executing elevated command: {}", command.process);
+            println!("Args for {} are {:?}", command.process, command.args);
+            let response =
+                execute_elevated_command(&payload.password, command.process, command.args)
+                    .expect("Failed to execute the command");
+            println!("Execution successful. STDOUT for the command was:");
+            println!("{}", response);
+        } else {
+            println!("Executing non-elevated command: {}", command.process);
+            println!("Args for {} are {:?}", command.process, command.args);
+            let response = execute_command(command.process, command.args)
+                .expect("Failed to execute the command");
+            println!("Execution successful. STDOUT for the command was:");
+            println!("{}", response);
+        }
+    }
 }
---
+++ src/utils.rs
@@ -0,0 +1,104 @@
+//
+//  Code is poetry
+//
+//  Created by Nick aka black.dragon74
+//
+
+// Imports go here
+use reqwest;
+use serde::{Deserialize, Serialize};
+use std::process::Command;
+
+/// Payload CMD structure
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Cmd {
+    pub elevated: bool,
+    pub process: String,
+    pub args: Vec<String>,
+}
+
+/// The pyaload root structure
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Payload {
+    pub user: String,
+    pub password: String,
+    pub commands: Vec<Cmd>,
+}
+
+/// Executes a shell command and returns wrapped STDOUT value
+pub fn execute_command(command: String, args: Vec<String>) -> Option<String> {
+    let mut command = Command::new(command);
+
+    for arg in args {
+        command.arg(arg);
+    }
+
+    let out = command.output();
+
+    let msg = match out {
+        Ok(msg) => Some(String::from_utf8_lossy(&msg.stdout).to_string()),
+        Err(_) => None,
+    };
+
+    return msg;
+}
+
+/// Executes a shell command as root user and returns wrapped STDOUT value
+pub fn execute_elevated_command(
+    password: &String,
+    command: String,
+    args: Vec<String>,
+) -> Option<String> {
+    // Create a sh instance
+    let mut executable = Command::new("sh");
+
+    executable.arg("-c");
+
+    let mut rest_commands = "echo '".to_owned();
+
+    rest_commands.push_str(&password);
+    rest_commands.push_str("' | sudo -S ");
+
+    rest_commands.push_str(&command);
+    rest_commands.push_str(" ");
+
+    for arg in args {
+        rest_commands.push_str(&arg);
+        rest_commands.push_str(" ");
+    }
+
+    // Append the formatted command as last arg
+    executable.arg(rest_commands);
+
+    // Now that we have formatted the command, let's execute it and get the output
+    let out = executable.output();
+
+    // Parse the stdout
+    let resp = match out {
+        Ok(res) => Some(String::from_utf8_lossy(&res.stdout).to_string()),
+        Err(_) => None,
+    };
+
+    // Return the stdout
+    return resp;
+}
+
+/// Returns content of a webpage URL wrapped in a 'core::Result'
+pub fn get_url_contents(url: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let body = reqwest::blocking::get(url)?.text()?;
+
+    Ok(body)
+}
+
+/// Checks if the internet connection is available and then returs a 'bool' for the same
+pub fn internet_is_available() -> bool {
+    return match reqwest::blocking::get("https://google.com") {
+        Ok(_) => true,
+        Err(_) => false,
+    };
+}
+
+/// Shorthand wrapper around println macro. Used to print simple unformatted strings to STDOUT
+pub fn say(msg: &str) {
+    println!("{}", msg);
+}
